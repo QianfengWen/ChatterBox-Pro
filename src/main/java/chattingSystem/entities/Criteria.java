8fb448bb8fa2664c26ce9ef3ab3d30f0369cbdf8
Ruohan Ren <amber.ren@mail.urotonto.ca>
2023-10-30 19:01:18 -0400

    added critetia.java

---
+++ src/main/java/chattingSystem/entities/Criteria.java
@@ -0,0 +1,24 @@
+package main.java.chattingSystem.entities;
+
+public class Criteria {
+    private boolean hasCharacter;
+    private boolean hasNumeric;
+    private int minLength;
+
+    public Criteria(boolean hascharacter, boolean hasSpecial){
+        this.hasCharacter = hascharacter;
+        this.hasNumeric = hasSpecial;
+        this.minLength = 8;
+    }
+
+    public boolean hasCharacter() {
+        return hasCharacter;
+    }
+    public boolean hasNumeric() {
+        return hasNumeric;
+    }
+    public int getMinLength() {
+        return minLength;
+    }
+}
+
