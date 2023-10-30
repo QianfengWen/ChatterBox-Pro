f70613f2c12bb5d1e6a019d3fc239a1f09049bb5
Helen Pan <helen.pan@mail.utoronto.ca>
2023-10-30 18:37:01 -0400

    User.java updated

--- src/main/java/chattingSystem/entities/User.java
+++ src/main/java/chattingSystem/entities/User.java
@@ -1,2 +1,29 @@
-public class User {
+package entity;
+
+import java.time.LocalDateTime;
+
+class User {
+    private final String username;
+    private final String userid;
+    private final String password;
+    private final LocalDateTime creationTime;
+    /**
+     * @param username
+     * @param userid
+     * @param password
+     */
+    User(String username, String userid, String password, LocalDateTime creationTime) {
+        this.username = username;
+        this.userid = userid;
+        this.password = password;
+        this.creationTime= creationTime;
+    }
+    @Override
+    public String getUsername(){return username};
+    @Override
+    public String getUserid(){return userid};
+    @Override
+    public String getPassword(){return password};
+    @Override
+    public LocalDateTime getCreationTime(){return creationTime};
 }
